58028bfcf84f951f4b95707f48dd447c97265bf6
= <=>
2024-02-26 17:37:37 +0000

    added scopes

---
+++ scopes.js
@@ -0,0 +1,19 @@
+
+// // Global Scope
+// // Local Scope
+
+// var firstName = "Ahmed"
+// var x = 10;
+// function hideMe() { // block
+//     console.log(x);
+//     var y = 20;
+//     console.log(x);
+//     console.log(y);
+// }
+
+
+
+// let lastName  = "Ahmed"  // declaring a variable and assigned/initialised value to it 
+// lastName = "Abdi" // resignment
+// console.log(lastName) // undefined
+
