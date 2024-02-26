5921409bcc56b85c64e488b17fcd4f9f106afd5c
= <=>
2024-02-26 17:17:08 +0000

    my first JS course

---
+++ variables.js
@@ -0,0 +1,89 @@
+// var lastName = "farah" // string
+// var firstName = "abdi"
+
+// var age = 20;  // number
+// var degreeTime = 4
+
+// var isMarried = true // boolean
+
+// console.log(lastName * firstName)
+
+// var boys = 5;
+
+// console.log(typeof(lastName))
+
+
+
+
+// function boys(param1, param2) {
+//     console.log("What boys like");
+//     console.log("Football");
+//     console.log("Cars");
+//     console.log("Games");
+//     console.log("Blue");
+//     console.log(param1);
+//     console.log(param2);
+
+// }
+
+// function girls(secret, socialMedia) { 
+//     console.log("What girls like");
+//     console.log("Makeup");
+//     console.log("Shopping");
+//     console.log("goingoutwithfriends");
+//     console.log("Pink");
+//     console.log(secret);
+//     console.log(socialMedia)
+// }
+
+// boys("bag", "Snapchat")
+// girls("bag","Snapchat")
+
+
+
+// function multiply(x, y) {// parameters
+//   var z = x * y
+//   return z;
+// }
+
+// console.log(multiply(10,30)) // arguments
+// console.log(multiply(5,3))
+// console.log(multiply(2,8))
+// console.log(multiply(6,9))
+// console.log(multiply(4,4))
+
+
+
+
+
+
+// var multiply = function(x,y) {
+//     var z = x * y;
+//     return z;
+// }
+
+// console.log(multiply(4,2))
+
+
+
+
+
+
+// console.log(firstName)
+
+
+
+// console.log("My name is  " + lastName )
+// console.log("My name is  " + lastName )
+// console.log("My name is  " + lastName )
+// console.log("My name is  " + lastName )
+
+var age = 50
+if (age >= 18 && age <= 20) {
+    console.log("I am 18 and 20 years old")
+}
+
+
+
+
+
